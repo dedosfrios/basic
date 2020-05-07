edbce70ec69a82547a2d4d5981347cd530e56d80
dedosfrios <jotagalvan@gmail.com>
2020-05-06 19:31:50 -0500

    update

--- prueba/js/script.js
+++ prueba/js/script.js
@@ -1,9 +1,3 @@
-var navTrigger = document.getElementsByClassName('nav-trigger')[0],
-    body = document.getElementsByTagName('body')[0];
+let headline = document.querySelectorAll("h1")[0];
 
-navTrigger.addEventListener('click', toggleNavigation);
-
-function toggleNavigation(event) {
-  event.preventDefault();
-  body.classList.toggle('nav-open');
-}
+headline.style.textTransform="uppercase";
