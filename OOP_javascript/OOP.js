4cb573e19ac75bc275ea9453d84c7644e3aec769
dedosfrios <jotagalvan@gmail.com>
2020-06-03 00:11:29 -0500

    otro objeto escrito

--- OOP_javascript/OOP.js
+++ OOP_javascript/OOP.js
@@ -28,5 +28,39 @@ const casaDeJota = new Casa ('Salota','Cocinita', 3, 2);
 // console.log (casaDeMorela.__proto__)
 // console.log(" ");
 
-console.log(Object)
-console.log(this.constructor)
+
+function Girls (name, placeOfMeet, age, fallinInLove){
+    this.name = name,
+    this.placeOfMeet = placeOfMeet,
+    this.age = age,
+    this.fallinInLove = fallinInLove,
+
+    this.didSheFellInLove = function(){
+        const self = this;
+        
+        let resultado = fallinInLove ? yesSheDid : notSheDidNot;
+        
+        function yesSheDid(){
+            return 'Yeap, ' + self.name + ', she fell in love'
+        }
+        function notSheDidNot(){
+            return 'Naa mate'
+        }        
+    return console.log(resultado());
+    }
+    this.whereImeetHer = function(){
+        console.log('you meet ' + this.name + ' in ' + this.placeOfMeet )
+    }
+}
+
+const sadith = new Girls('Sadith', 'Karlas Place', 22, true)
+const karoll = new Girls('Karoll','Leo Design', 19 , false)
+
+console.log(sadith.didSheFellInLove())
+console.log(karoll.didSheFellInLove())
+// console.log(sadith.whereImeetHer())
+
+
+// console.log(sadith)
+
+//El objeto windows se crea cuando se renderiza el DOM tree, el 
